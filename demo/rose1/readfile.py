7b18421a11bbe51c09b2f61374a52dbafd730797
unknown <fanpengfei@360buyad.local>
2020-01-15 10:20:04 +0800

    fw

--- demo/rose1/readfile.py
+++ demo/rose1/readfile.py
@@ -1,40 +1,64 @@
+#!/usr/bin/python
 # -*- coding: UTF-8 -*-
+
+import sys
+
+#filename = sys.argv[1]
 filename = r"D:\source\InventWithPython\demo\rose1\usbPerf_itl_generic_64_smp_Dynamic_DI_28614.ini"
+
 f = open(filename, "r+")
 dics = {}
 mode = 0
 
 while 1:
-    line = f.readline().strip()
-    if not line:
-        break
+	line = f.readline()
+	if not line:
+		break;
+	line = line.strip()
+	if not line:
+		continue
 
-    if (line[0] == '['):
-        # next status parse
-        if (line.find('TestCase_') > 0):
-            mode = 2
-        else:
-            mode = 1
+	if (line[0] == '['):
+		# next status parse
+		if (line.find('TestCase_') > 0):
+			mode = 2
+		else:
+			mode = 1
 
-    if (mode == 1):
-        line = f.readline().strip()
-        (key, value) = line.split('=', 2)
-        dics[key]=value
-    elif (mode==2):
-        tempDic={}
-        tempKey = line[1:len(line)-1]
-        while 1:
-            line = f.readline().strip()
-            if not line:
-                dics[tempKey] = tempDic
-                break
-            (key, value) = line.split('=', 2)
-            tempDic[key]=value
-        #read 
-    mode = 0
+	if (mode == 1):
+		line = f.readline()
+		if not line:
+			break
+		line = line.strip()
+		if not line:
+			continue;
+
+		(key, value) = line.split('=', 2)
+		dics[key]=value
+	elif (mode==2):
+		tempDic={}
+		tempKey = line[1:len(line)-1]
+		i = 0
+		while(1):
+			line = f.readline()
+			if not line:
+				break;
+			line = line.strip()
+			if not line:
+				continue
+
+			if i == 4:
+				dics[tempKey] = tempDic
+				break
+			(key, value) = line.split('=', 2)
+			tempDic[key]=value
+			i = i + 1
+	mode = 0
 
 print(dics)
 
-# 关闭文件
 f.close()
+
+
+
 
